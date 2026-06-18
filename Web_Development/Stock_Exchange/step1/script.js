a22fca0341bf1e072c31aadaa5c72323f0dac235
Eliya Shlomo <eliyashlomo@h-MacBook-Pro-sl-Eliya.local>
2026-06-18 19:59:25 +0300

    step 3

--- Web_Development/Stock_Exchange/step1/script.js
+++ Web_Development/Stock_Exchange/step1/script.js
@@ -4,9 +4,9 @@ class Model{
 
     constructor(){}
     async getData(value){
-        const searchResponse = await fetch(`https://financialmodelingprep.com/stable/search-symbol?query=${value}&limit=10&apikey=WtKB5TSUJbVGntrakVVCNPSX5qIqGNji`)
+        const searchResponse = await fetch(`https://financialmodelingprep.com/stable/search-symbol?query=${value}&apikey=WtKB5TSUJbVGntrakVVCNPSX5qIqGNji`)
         const Data           = await searchResponse.json()
-        let nasdaqCompanies = []
+        let nasdaqCompanies  = []
 
         for(let i=0; i<Data.length;i++){
             if(nasdaqCompanies.length === 10){
@@ -16,7 +16,26 @@ class Model{
                 nasdaqCompanies.push(Data[i])
             }
         }
-        return nasdaqCompanies
+
+        const symbolsArray  = nasdaqCompanies.map(company => company.symbol);
+        const symbolsString = symbolsArray.join(',');
+        const response      = await fetch(`https://financialmodelingprep.com/stable/company-profile?symbol=${symbolsString}&apikey=WtKB5TSUJbVGntrakVVCNPSX5qIqGNji`);
+        const dataProfiles  = await response.json()
+
+        const profiles      = dataProfiles.companyProfile || []
+
+        const finalResults  = nasdaqCompanies.map(company => {
+            const profile = profiles.find(p => p.symbol === company.symbol)
+            
+            return {
+                name: company.name,
+                symbol: company.symbol,
+                image: profile ? profile.image : '', 
+                changesPercentage: profile ? profile.changesPercentage : '0'
+            }
+        })
+
+        return finalResults
     }
 }
 
@@ -44,18 +63,33 @@ class View{
 
             for (let i = 0; i < results.length; i++) {
                 const company = results[i];
+
+                const change = parseFloat(company.changesPercentage);
+                let color = "";
+                let sign = "";
+                
+                if (change >= 0) {
+                    color = "#22c55e"; 
+                    sign = "+";
+                } else {
+                    color = "#ef4444"; 
+                    sign = ""; 
+                }
                 $("#results-list").append(`
                     <li class="result-item">
+                        <img class="company-image" src="${company.image}" alt="logo">
                         <a href="company.html?symbol=${company.symbol}" class="result-link">
-                            <span class="company-name">${company.name}</span>
-                            <span class="company-symbol">${company.symbol}</span>
-                        </a>
+                        <span class="company-name">${company.name}</span>
+                        <span class="company-symbol">${company.symbol}</span>
+                        <span class="company-change" style="color: ${color}">${sign}${change}%</span>
                     </li>
                 `);
             }
 
         }
     }
+
+
 }
 
 const app = new View();
